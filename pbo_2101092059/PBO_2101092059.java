d53bd63f97683a61f7ec7ca7846a536b0c9a6120
mustika chairani <111229773+mustikachairani@users.noreply.github.com>
2023-01-27 23:28:01 +0700

    Add files via upload

---
+++ pbo_2101092059/PBO_2101092059.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo_2101092059;
+
+/**
+ *
+ * @author user
+ */
+public class PBO_2101092059 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    
+}
